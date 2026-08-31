d298d420e2716368df97b58fadf08fa9c07ef348
kesavansundar06 <kesavansundar06@gmail.com>
2026-08-31 10:11:37 +0530

    Check production MongoDB environment

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,15 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log(
+      "Mongo URI type:",
+      process.env.MONGO_URI?.startsWith("mongodb+srv://")
+        ? "ATLAS"
+        : "LOCAL/UNKNOWN"
+    );
+
     await mongoose.connect(process.env.MONGO_URI);
+
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
